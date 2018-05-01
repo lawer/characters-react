728dc7ef9288e1c7838b1139f0e61056bfd14e51
carlesgm <carlesgm@gmail.com>
2018-05-01 23:52:05 +0200

    Character List

--- src/App.js
+++ src/App.js
@@ -1,18 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Link, Route} from "react-router-dom";
+import axios from 'axios';
+
+
+var urlApi = "https://secure-cliffs-32652.herokuapp.com/api"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/" component={CharacterList}/>
+        <Route path="/characters/:id" component={CharacterDetail}/>
+      </div>
+    );
+  }
+}
+
+class CharacterList extends Component {
+  state = {
+    characters: []
+  }
+
+  componentDidMount() {
+    axios.get(`${urlApi}/characters`)
+      .then(res => {
+        const characters = res.data.characters;
+        this.setState({characters});
+      })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>GoT Characters</h1>
+
+        <Link to="/characters/add" className="add btn btn-primary">Add Character</Link>
+
+        <table className="table table-striped">
+          <thead className="thead-dark">
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Alive</th>
+            <th>Culture</th>
+            <th>House</th>
+            <th>Actions</th>
+          </tr>
+          </thead>
+
+          <tbody>
+          {this.state.characters.map(
+            character =>
+              <tr>
+                <td className="align-middle">
+                  <Link to={"/characters/" + character._id}>
+                    <img src={character.image} alt="" className="img-fluid"/>
+                  </Link>
+                </td>
+                <td className="align-middle">
+                  <Link to={"/characters/" + character._id}>
+                    {character.name}
+                  </Link>
+                </td>
+                <td className="align-middle text-center">
+                  {character.alive}
+                </td>
+                <td className="align-middle">
+                  {character.culture}
+                </td>
+                <td className="align-middle">
+                  {character.house}
+                </td>
+                <td>
+                </td>
+              </tr>
+          )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class CharacterDetail extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Character</h1>
       </div>
     );
   }

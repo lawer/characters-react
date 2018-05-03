8d1c8c1ec4a93b20abd31f8ea413335c94d2c803
carlesgm <carlesgm@gmail.com>
2018-05-03 10:42:33 +0200

    Add Character

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Link, Route, Switch} from "react-router-dom";
 import axios from 'axios';
+import {Form, Text} from "react-form";
 
 
 var urlApi = "https://secure-cliffs-32652.herokuapp.com/api"
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={CharacterList}/>
+          <Route exact path="/characters/add/" component={AddCharacter}/>
           <Route path="/characters/:id/" component={CharacterDetail}/>
         </Switch>
       </div>
@@ -48,54 +50,55 @@ class CharacterList extends Component {
   }
 }
 
-class CharacterTable extends Component {
+function CharacterTable(props) {
+  return (
+    <table className="table table-striped">
+      <thead className="thead-dark">
+      <tr>
+        <th>Image</th>
+        <th>Name</th>
+        <th>Alive</th>
+        <th>Culture</th>
+        <th>House</th>
+        <th>Actions</th>
+      </tr>
+      </thead>
 
-  render() {
-    return (
-      <table className="table table-striped">
-        <thead className="thead-dark">
-        <tr>
-          <th>Image</th>
-          <th>Name</th>
-          <th>Alive</th>
-          <th>Culture</th>
-          <th>House</th>
-          <th>Actions</th>
-        </tr>
-        </thead>
+      <tbody>
+      {props.characters.map(
+        character => <CharacterRow character={character}/>
+      )}
+      </tbody>
+    </table>
+  );
+}
 
-        <tbody>
-        {this.props.characters.map(
-          character =>
-            <tr>
-              <td className="align-middle">
-                <Link to={"/characters/" + character._id}>
-                  <img src={character.image} alt="" className="img-fluid"/>
-                </Link>
-              </td>
-              <td className="align-middle">
-                <Link to={"/characters/" + character._id}>
-                  {character.name}
-                </Link>
-              </td>
-              <td className="align-middle text-center">
-                {character.alive}
-              </td>
-              <td className="align-middle">
-                {character.culture}
-              </td>
-              <td className="align-middle">
-                {character.house}
-              </td>
-              <td>
-              </td>
-            </tr>
-        )}
-        </tbody>
-      </table>
-    );
-  }
-
+function CharacterRow({character}) {
+  return (
+    <tr>
+      <td className="align-middle">
+        <Link to={"/characters/" + character._id}>
+          <img src={character.image} alt="" className="img-fluid"/>
+        </Link>
+      </td>
+      <td className="align-middle">
+        <Link to={"/characters/" + character._id}>
+          {character.name}
+        </Link>
+      </td>
+      <td className="align-middle text-center">
+        {character.alive}
+      </td>
+      <td className="align-middle">
+        {character.culture}
+      </td>
+      <td className="align-middle">
+        {character.house}
+      </td>
+      <td>
+      </td>
+    </tr>
+  )
 }
 
 class CharacterDetail extends Component {
@@ -127,6 +130,63 @@ class CharacterDetail extends Component {
           <li>Culture: {this.state.character.culture}</li>
           <li>House: {this.state.character.house}</li>
         </ul>
+      </div>
+    );
+  }
+}
+
+class AddCharacter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      character: {}
+    };
+  }
+
+  addCharacter(character) {
+    this.setState({character});
+
+    axios.post(`${urlApi}/characters`, character)
+      .then(res => {
+        this.props.history.push("/")
+      })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Add Character</h1>
+
+        <Form onSubmit={character => this.addCharacter(character)}>
+          {formApi => (
+            <form onSubmit={formApi.submitForm} id="form1" className="mb-4">
+              <label htmlFor="name">Name</label>
+              <Text field="name" id="name"/>
+              <br/>
+
+              <label htmlFor="image">Image</label>
+              <Text  field="image" id="image"/>
+              <br/>
+
+              <label htmlFor="alive">Alive</label>
+              <Text field="alive" id="alive"/>
+              <br/>
+
+              <label htmlFor="culture">Culture</label>
+              <Text field="culture" id="culture"/>
+              <br/>
+
+              <label htmlFor="house">House</label>
+              <Text field="house" id="house"/>
+              <br/>
+
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          )}
+        </Form>
       </div>
     );
   }

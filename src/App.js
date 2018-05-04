2b5459d96ab7e1f9b966b31568c191a696e6db5a
carlesgm <carlesgm@gmail.com>
2018-05-04 10:23:08 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+          <Link to="/" className="navbar-brand">Characters</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                  aria-controls="navbarNav"
+                  aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/register" className="nav-link">Register</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">Login</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
         <Switch>
           <Route exact path="/" component={CharacterList}/>
           <Route exact path="/characters/add/" component={AddCharacter}/>
